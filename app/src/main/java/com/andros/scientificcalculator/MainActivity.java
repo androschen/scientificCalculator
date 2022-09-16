5124afd57d8da8eba9df4cd5248aa05930851087
androschen <androscc7@gmail.com>
2022-09-16 17:53:05 +0700

    Add plus button function

--- app/src/main/java/com/andros/scientificcalculator/MainActivity.java
+++ app/src/main/java/com/andros/scientificcalculator/MainActivity.java
@@ -9,11 +9,17 @@ import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
+import java.util.Stack;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView history;
     EditText finalCount;
     String input = "";
+    String numb="";
+    Stack<Integer>number = new Stack<>();
+    Stack<Character>op = new Stack<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,40 +29,52 @@ public class MainActivity extends AppCompatActivity {
         finalCount = findViewById(R.id.finalCount);
 
 
+
+
     }
 
     public void button7Pressed(View view) {
         input += "7";
+        numb += "7";
         finalCount.setText(input);
     }
 
     public void button8Pressed(View view) {
         input += "8";
+        numb += "8";
         finalCount.setText(input);
     }
 
     public void button9Pressed(View view) {
         input += "9";
+        numb += "9";
         finalCount.setText(input);
     }
 
     public void buttonDeletePressed(View view) {
-        input=input.substring(0,input.length()-1);
+        if(op.empty()){
+            input=input.substring(0,input.length()-1);
+            numb=input;
+        }
+
         finalCount.setText(input);
     }
 
     public void button4Pressed(View view) {
         input += "4";
+        numb += "4";
         finalCount.setText(input);
     }
 
     public void button5Pressed(View view) {
         input += "5";
+        numb += "5";
         finalCount.setText(input);
     }
 
     public void button6Pressed(View view) {
         input += "6";
+        numb += "6";
         finalCount.setText(input);
     }
 
@@ -65,20 +83,45 @@ public class MainActivity extends AppCompatActivity {
 
     public void button1Pressed(View view) {
         input += "1";
+        numb += "1";
         finalCount.setText(input);
     }
 
     public void button2Pressed(View view) {
         input += "2";
+        numb += "2";
         finalCount.setText(input);
     }
 
     public void button3Pressed(View view) {
         input += "3";
+        numb += "3";
         finalCount.setText(input);
     }
 
     public void buttonAddPressed(View view) {
+        input+="+";
+        finalCount.setText(input);
+        if(op.empty()){
+            String num = numb;
+            numb = "";
+            number.push(Integer.parseInt(num));
+            op.push('+');
+        }else{
+            Character ops = op.peek();
+            if(ops=='+'){
+                Integer temp = number.peek()+Integer.parseInt(numb);
+                input = String.valueOf(temp+"+");
+                finalCount.setText(input);
+                number.pop();
+                number.push(temp);
+                op.push('+');
+                numb="";
+                System.out.println(temp);
+            }
+            op.pop();
+        }
+        System.out.println(input+" "+numb);
     }
 
     public void buttonMultiplePressed(View view) {
@@ -86,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void button0Pressed(View view) {
         input += "0";
+        numb += "0";
         finalCount.setText(input);
     }
 
